295cb9c4802603c520b7495de81f7aba7013be02
Haronchick <haron.www@mail.ru>
2017-11-28 16:19:02 +0300

    start

---
+++ src/by/part1/homework/HomeWork4.java
@@ -0,0 +1,11 @@
+package by.part1.homework;
+
+public class HomeWork4 {
+
+  //  Complete the program which takes date in number format and then makes its addition of the digits until
+// the last digit remains single and the print the number as lucky number.
+// example if input is 777 then 7+7+7 =21 = 2+1 =3 so the answer is 3.
+  public static void main(String[] args) {
+
+  }
+}
